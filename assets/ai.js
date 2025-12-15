e5c3082fd6123e37e5f82c832b58a892f05022d7
dr-luis-moralesv <luis.moralesv@uaq.edu.mx>
2025-12-15 08:54:27 -0600

    Update ai.js

--- assets/ai.js
+++ assets/ai.js
@@ -1,6 +1,6 @@
 async function consultarIA() {
   const response = await fetch(
-    "https://script.google.com/macros/s/1k47RHCuZn1TgpoZj5gKnMcB-eOO6PW4l6ZlI0n6RmW0lLw0kfcRT2pZc/exec",
+    "https://script.google.com/macros/s/AKfycby3y446NKLPaetfIEet8deUOaxwC_TQCaZREULL3as2fUSn38pjt1Ve5oJc4QXIVwfs9g/exec",
     {
       method: "POST",
       body: JSON.stringify({
